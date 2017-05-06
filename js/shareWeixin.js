3f1ee5d972a835ea83ee80f7a38787e61e9d1cd9
sylvia <935152050@qq.com>
2017-05-06 20:49:07 +0800

    调整页面编码问题

--- js/shareWeixin.js
+++ js/shareWeixin.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-//相关链接微信分享
-$(".wx").hover(function(){
-$(".wx_box").fadeIn(200);
-},function(){
-$(".wx_box").fadeOut(200);
-});
+//寰俊鍥剧墖
+    $(".wx").hover(function () {
+        $(".wx_box").fadeIn(200);
+    }, function () {
+        $(".wx_box").fadeOut(200);
+    });
 });
